dda566414f3aa2a3a76f168af15a5ac1f2a77ebf
nasrinlida <nasrinakterlida@gmail.com>
2021-02-15 15:47:00 +0600

    The app is completed which is now able to convert from any unit of time into other units.

--- UnitConversionSwiftUI/ContentView.swift
+++ UnitConversionSwiftUI/ContentView.swift
@@ -9,18 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     @State var inputNumber = ""
-    @State var inputUnit = 1
-    @State var outputUnit = 1
+    @State var inputUnit = 0
+    @State var outputUnit = 0
     
     let unitList = ["Second", "Minute", "Hour", "Day"]
+    
+    //Computed property inputAmount will return user's input
+    //by converting into second taking second as the single base unit
+    var inputAmount: Double {
+        let amount = Double(inputNumber) ?? 0
+        
+        if unitList[inputUnit] == "Minute"{
+            let minuteToSecond = amount * 60
+            return minuteToSecond
+            
+        } else if unitList[inputUnit] == "Hour" {
+            let hourToSecond = amount * 60 * 60
+            return hourToSecond
+            
+        } else if unitList[inputUnit] == "Day" {
+            let dayToSecond = amount * 24 * 60 * 60
+            return dayToSecond
+        }
+        else {
+            return amount
+        }
+    }
+    
+    var outputAmount: Double {
+        if unitList[outputUnit] == "Minute" {
+            let secondToMinute = inputAmount / 60
+            return secondToMinute
+            
+        } else if unitList[outputUnit] == "Hour" {
+            let secondToHour = inputAmount / 60 / 60
+            return secondToHour
+            
+        } else if unitList[outputUnit] == "Day" {
+            let secondToDay = inputAmount / 24 / 60 / 60
+            return secondToDay
+            
+        } else {
+            return inputAmount
+        }
+    }
+    
+    
     
     var body: some View {
         NavigationView {
             Form {
-                Section {
+                Section(header: Text("Enter your desired amount: ")) {
                     TextField("Input Number", text: $inputNumber)
                         .keyboardType(.decimalPad)
                 }
+                .textCase(nil)
                 
                 Section(header: Text("Choose the unit you want to convert from: ")) {
                     Picker("Unit of Input", selection: $inputUnit) {
@@ -42,9 +85,10 @@ struct ContentView: View {
                 }
                 .textCase(nil)
                 
-                Section {
-                    Text("\(inputNumber)")
+                Section(header: Text("Output")) {
+                    Text(String(format: "%.2f", outputAmount))
                 }
+                .textCase(nil)
             }
             .navigationBarTitle("UnitConversion")
         }
